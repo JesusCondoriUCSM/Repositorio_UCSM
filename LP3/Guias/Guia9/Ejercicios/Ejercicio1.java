2ce2841e918f359d704eb776a2a1a29f31d236f5
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2024-11-19 01:08:41 -0500

    Update Ejercicio1.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- LP3/Guias/Guia9/Ejercicios/Ejercicio1.java
+++ LP3/Guias/Guia9/Ejercicios/Ejercicio1.java
@@ -1,1 +1,39 @@
+public Ejercicio1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(3, 2, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Ingrese su primer nombre");
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ingrese su segundo nombre");
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Hola ");
+		contentPane.add(lblNewLabel_2);
+		
+		JButton btnNewButton = new JButton("Saludar");
+		btnNewButton.addActionListener(new ActionListener() {
+	
+			public void actionPerformed(ActionEvent e) {
+				String primer_n = textField.getText();
+	            String segundo_n = textField_1.getText();
+	            
+				lblNewLabel_2.setText("Hola "+ primer_n +" "+ segundo_n+"!!");
+			}
+		});
+		contentPane.add(btnNewButton);
+	}

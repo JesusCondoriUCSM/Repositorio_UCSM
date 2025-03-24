1dd1b2e76c27d281d5391a767728b5093b868456
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-03-23 19:42:18 -0500

    Create Operable.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

---
+++ Algoritmos_ED/S1/F1/Guia2/Ejercicios/Ejercicio1/Operable.java
@@ -0,0 +1,9 @@
+package AED_1F2_E;
+
+public interface Operable<N extends Number>{
+	
+	public N suma(N operando1, N operando2);
+	public N resta(N operando1, N operando2);
+	public N producto ( N operando1, N operando2);
+	public N division (N operando1, N operando2);
+}

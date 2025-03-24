319e80650b63c7c6d149bc67fea6daae70187077
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-03-23 19:43:33 -0500

    Create OperacionesMatDouble.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

---
+++ Algoritmos_ED/S1/F1/Guia2/Ejercicios/Ejercicio1/OperacionesMatDouble.java
@@ -0,0 +1,25 @@
+package AED_1F2_E;
+
+public class OperacionesMatDouble implements Operable<Double>{
+
+	@Override
+	public Double suma(Double operando1, Double operando2) {
+		return operando1 + operando2;
+	}
+	
+	@Override
+	public Double resta(Double operando1, Double operando2) {
+		return operando1 - operando2;
+	}
+	
+	@Override
+	public Double producto(Double operando1, Double operando2) {
+		return operando1 * operando2;
+	}
+	
+	@Override
+	public Double division(Double operando1, Double operando2) {
+		return operando1 / operando2;
+	}
+
+}

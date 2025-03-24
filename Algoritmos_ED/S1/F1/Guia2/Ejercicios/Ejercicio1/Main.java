1460fb5869d83a5f25352f60d6cd46c85c485657
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-03-23 19:55:22 -0500

    Update Main.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- Algoritmos_ED/S1/F1/Guia2/Ejercicios/Ejercicio1/Main.java
+++ Algoritmos_ED/S1/F1/Guia2/Ejercicios/Ejercicio1/Main.java
@@ -10,15 +10,17 @@ public class Main {
 		System.out.println(operacionesMatBInteger.suma(1, 100));
 		*/
 		OperacionesMatDouble operacionesMatDouble = new OperacionesMatDouble();
-		
+		//Operaciones tipo Double
 		System.out.println(operacionesMatDouble.suma(1.0, 100.0));
-		
 		System.out.println(operacionesMatDouble.resta(100.0, 100.0));
-
 		System.out.println(operacionesMatDouble.producto(1.0, 100.0));
-
 		System.out.println(operacionesMatDouble.division(1.0, 100.0));
 
+		//Operaciones tipo Integer
+		System.out.println(operacionesMatBInteger.suma(1, 100));
+		System.out.println(operacionesMatBInteger.resta(5, 100));
+		System.out.println(operacionesMatBInteger.producto(2, 100));
+		System.out.println(operacionesMatBInteger.division(200, 100));
 	}
 	
 }

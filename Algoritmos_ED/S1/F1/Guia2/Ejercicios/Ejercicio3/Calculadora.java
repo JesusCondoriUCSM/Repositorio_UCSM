8f12139b8a359ec1dc72524b7cc4b63b33d563ca
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-03-23 19:46:10 -0500

    Create Calculadora.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

---
+++ Algoritmos_ED/S1/F1/Guia2/Ejercicios/Ejercicio3/Calculadora.java
@@ -0,0 +1,43 @@
+package AED_1F2_E3;
+
+public class Calculadora {
+	
+	//Suma
+	public static <T extends Number> T suma(T n1, T n2) {
+		
+		return (T) Double.valueOf(n1.doubleValue()+n2.doubleValue());
+	}
+	//Resta
+	public static <T extends Number> T resta(T n1, T n2) {
+		
+		return (T) Double.valueOf(n1.doubleValue()-n2.doubleValue());
+	}
+	//Producto
+	public static <T extends Number> T producto(T n1, T n2) {
+		
+		return (T) Double.valueOf(n1.doubleValue()*n2.doubleValue());
+	}
+	//Division
+	public static <T extends Number> T division(T n1, T n2) {
+		
+		return (T) Double.valueOf(n1.doubleValue()/n2.doubleValue());
+	}
+	//Potencia
+	public static <T extends Number> T potencia(T n1, T n2) {
+		
+		Double verificar = (Double) Double.valueOf(n2.doubleValue());
+		if(verificar==0) {
+			return (T) Double.valueOf(1);
+		}
+		return (T) Double.valueOf(Math.pow(n1.doubleValue(),n2.doubleValue()));
+
+	}
+	//Raiz Cuadrada
+	public static <T extends Number> T raizCuadrada(T n1) {
+		return (T) Double.valueOf(Math.sqrt(n1.doubleValue()));
+	}
+	//Raiz Cubica
+	public static <T extends Number> T raizCubica(T n1) {
+		return (T) Double.valueOf(Math.cbrt(n1.doubleValue()));
+	}
+}

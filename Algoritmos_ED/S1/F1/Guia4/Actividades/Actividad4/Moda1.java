3abc3a602fcfc0c0aaf8d2e6f0b0c38ee25aa24b
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-04-12 19:54:52 -0500

    Update Moda1.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- Algoritmos_ED/S1/F1/Guia4/Actividades/Actividad4/Moda1.java
+++ Algoritmos_ED/S1/F1/Guia4/Actividades/Actividad4/Moda1.java
@@ -1,38 +1,37 @@
 public class Moda1 {
 	public static int moda1(int array[]) {
 		
-		int first=0;                                            O(1)
-		int end=array.length-1;                 O(1)
+		int first=0;
+		int end=array.length-1;
 		
-		if(first == end)                                    O(1)
-			return array[first];           O(1)
-		int moda = array[first];                  O(1)
-		int maxfrec = frecuencia(array, first, end, moda);      O(n)
+		if(first == end)
+			return array[first];
+		int moda = array[first];
+		int maxfrec = frecuencia(array, first, end, moda);
 		
-		for(int i= first+1;i<end;i++) {                 O(n)
-			int frec=frecuencia(array,i,end,array[i]);      O(n)
-			if(frec>maxfrec) {                  O(1)
-				maxfrec=frec;         O(1)
-				moda=array[i];       O(1)
+		for(int i= first+1;i<end;i++) {
+			int frec=frecuencia(array,i,end,array[i]);
+			if(frec>maxfrec) {
+				maxfrec=frec;
+				moda=array[i];
 			}
 		}
-		return moda;                                  O(1)
+		return moda;
 	}
-
 	private static int frecuencia(int []array, int first, int end, int ele) {
-		if(first > end) return 0;             O(1)
-		int suma=0;                                  O(1)
-		for(int i=first; i<=end;i++)       O(n)
-			if(array[i]==ele)           O(1)
-				suma++;        O(1)
-		return suma;                               O(1)
+		if(first > end) return 0;
+		int suma=0;
+		for(int i=first; i<=end;i++) 
+			if(array[i]==ele)
+				suma++;
+		return suma;
 		
 	}
-                 public static void main(String []args) {
+	
+	public static void main(String []args) {
 		Moda1 moda = new Moda1();
 		int lista[]= {1,3,3,4,5,6};
 		int m=moda.moda1(lista);
 		System.out.println("La moda es: "+m);
-	  }
-
+	}
 }

eaed372635bc07aa7611b1538ca6bb42c93c2ef3
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-05-25 17:40:50 -0500

    Update TesttAVL.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- Algoritmos_ED/S1/F2/Guia8/Actividad/avltree/TesttAVL.java
+++ Algoritmos_ED/S1/F2/Guia8/Actividad/avltree/TesttAVL.java
@@ -11,6 +11,11 @@ public class TestAVL {
             arbol.insert(4);
             arbol.insert(5);
             arbol.insert(6);
+            arbol.insert(7);
+            arbol.insert(8);
+            arbol.insert(9);
+            arbol.insert(1);
+            arbol.insert(10);
             arbol.drawBST();
             System.out.println(arbol);
         } catch (ItemDuplicated i) {

fe3ba15df8b0d40c5c27459743d227d691012ff0
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-05-25 19:44:09 -0500

    Create TestAVL.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

---
+++ Algoritmos_ED/S1/F2/Guia8/CodigoCompleto/avltree/TestAVL.java
@@ -0,0 +1,44 @@
+package Guia8.Actividad.avltree;
+
+import Guia8.Actividad.Exceptions.*;
+
+public class TestAVL {
+    public static void main(String[] args) {
+        try {
+            AVLTree<Integer> arbol = new AVLTree<>();
+            arbol.insert(2);
+            arbol.insert(3);
+            arbol.insert(4);
+            arbol.insert(5);
+            arbol.insert(6);
+            arbol.insert(7);
+            arbol.insert(8);
+            arbol.insert(9);
+            arbol.insert(1);
+            arbol.insert(10);
+            arbol.drawBST();
+            System.out.println(arbol);
+
+            System.out.println("Elemento encontrado: " + arbol.search(2)); //Buscar un elemento que no esta en el arbol
+            System.out.println("Elemento encontrado: " + arbol.search(10));
+
+            System.out.println("Altura del subarbol: " + arbol.height(9));
+            System.out.println("Altura del subarbol: " + arbol.height(2));
+
+            arbol.delete(10);
+            arbol.drawBST();
+
+            System.out.println(arbol);
+
+            arbol.recorridoAmplitud();
+
+            arbol.preOrder();
+        } catch (ItemDuplicated i) {
+            System.out.println(i.getMessage());
+        } catch (ItemNoFound item) {
+            System.out.println(item.getMessage());
+        } catch (ExceptionIsEmpty e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}

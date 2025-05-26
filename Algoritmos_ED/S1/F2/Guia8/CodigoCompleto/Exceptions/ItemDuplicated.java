b3f36cb7170d530d07398de89adb110ac7019d71
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-05-25 19:46:44 -0500

    Create ItemDuplicated.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

---
+++ Algoritmos_ED/S1/F2/Guia8/CodigoCompleto/Exceptions/ItemDuplicated.java
@@ -0,0 +1,11 @@
+package Guia8.Actividad.Exceptions;
+
+public class ItemDuplicated extends Exception {
+    public ItemDuplicated(String msg) {
+        super(msg);
+    }
+    public ItemDuplicated() {
+        super();
+    }
+
+}

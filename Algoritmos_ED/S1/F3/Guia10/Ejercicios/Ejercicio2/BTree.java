f943d4f94427796d75c4f7edd24629fe0ffd132f
JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>
2025-06-20 02:39:51 -0500

    Update BTree.java
    Signed-off-by: JesusCondoriUCSM <164430499+JesusCondoriUCSM@users.noreply.github.com>

--- Algoritmos_ED/S1/F3/Guia10/Ejercicios/Ejercicio2/BTree.java
+++ Algoritmos_ED/S1/F3/Guia10/Ejercicios/Ejercicio2/BTree.java
@@ -1,22 +1,21 @@
 @SuppressWarnings("unchecked")
-	public RegistroEstudiante buscarEstudiante(int codigo) {
-	    RegistroEstudiante clave = new RegistroEstudiante(codigo, "");
-	    return buscarEstudianteRec(root, clave);
+public RegistroEstudiante buscarEstudiante(int codigo) {
+	RegistroEstudiante clave = new RegistroEstudiante(codigo, "");
+	return buscarEstudianteRec(root, clave);
+}
+
+@SuppressWarnings("unchecked")
+private RegistroEstudiante buscarEstudianteRec(BNode<E> node, RegistroEstudiante target) {
+	if (node == null) return null;
+
+	int i = 0;
+	while (i < node.count && target.compareTo((RegistroEstudiante) node.keys.get(i)) > 0) i++;
+
+	if (i < node.count && target.compareTo((RegistroEstudiante) node.keys.get(i)) == 0) {
+		System.out.println(target.getCodigo() + " se encuentra en el nodo " + node.idNode + " en la posición " + i);
+	return (RegistroEstudiante) node.keys.get(i);
 	}
 
-	@SuppressWarnings("unchecked")
-	private RegistroEstudiante buscarEstudianteRec(BNode<E> node, RegistroEstudiante target) {
-	    if (node == null) return null;
-
-	    int i = 0;
-	    while (i < node.count && target.compareTo((RegistroEstudiante) node.keys.get(i)) > 0) i++;
-
-	    if (i < node.count && target.compareTo((RegistroEstudiante) node.keys.get(i)) == 0) {
-	        System.out.println(target.getCodigo() + " se encuentra en el nodo " + node.idNode + " en la posición " + i);
-	        return (RegistroEstudiante) node.keys.get(i);
-	    }
-
-	    if (node.childs.get(i) == null) return null;
-
+	if (node.childs.get(i) == null) return null;
 	    return buscarEstudianteRec(node.childs.get(i), target);
-	}
+}
